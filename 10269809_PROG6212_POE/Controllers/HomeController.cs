277726ec9a0b78f7e702bfbdf1c664cb2af5de4b
Yasin Davids <yasindavids1134@gmail.com>
2024-09-10 19:46:35 +0200

    Add project files.

---
+++ 10269809_PROG6212_POE/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using _10269809_PROG6212_POE.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _10269809_PROG6212_POE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult SignUp()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 10269809_PROG6212_POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _10269809_PROG6212_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
