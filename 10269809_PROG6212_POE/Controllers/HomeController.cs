def87804a1d16a2379d2bdb20cdc3576c4c194ec
Yasin Davids <yasindavids1134@gmail.com>
2024-10-19 20:30:35 +0200

    Got rid of login functionality, seemed unnecessary for the project scope

--- 10269809_PROG6212_POE/Controllers/HomeController.cs
+++ 10269809_PROG6212_POE/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using _10269809_PROG6212_POE.Models;
+using _10269809_PROG6212_POE.Views.Home;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System.Diagnostics;
 
 namespace _10269809_PROG6212_POE.Controllers
@@ -7,6 +9,35 @@ namespace _10269809_PROG6212_POE.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(DashBoardModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check if the file is not null and has content
+                if (model.FileUpload != null && model.FileUpload.Length > 0)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", model.FileUpload.FileName);
+
+                    // Using OpenReadStream() to read the file contents
+                    using (var stream = model.FileUpload.OpenReadStream())
+                    {
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await stream.CopyToAsync(fileStream);  
+                        }
+                    }
+                }
+
+                return View("Success");
+            }
+
+
+            return View(model);
+        }
+
+
 
         public HomeController(ILogger<HomeController> logger)
         {
---
+++ 10269809_PROG6212_POE/Models/DashBoardModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace _10269809_PROG6212_POE.Models
+{
+    public class DashBoardModel
+    {
+        [Required(ErrorMessage = "Please enter hours worked.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Hours worked must be a number.")]
+        public int HoursWorked { get; set; }
+
+        [Required(ErrorMessage = "Please enter an hourly rate.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Hourly rate must be a number.")]
+        public int HourlyRate { get; set; }
+
+        public string Notes { get; set; }
+
+        public IFormFile FileUpload { get; set; }
+
+
+
+
+    }
+}
